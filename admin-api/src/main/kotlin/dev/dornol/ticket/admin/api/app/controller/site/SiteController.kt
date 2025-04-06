59610478f0fb479e7ac033d3076084f4598e3467
김동혁 <ucceoum@gmail.com>
2025-04-06 20:48:03 +0900

    test

--- admin-api/src/main/kotlin/dev/dornol/ticket/admin/api/app/controller/site/SiteController.kt
+++ admin-api/src/main/kotlin/dev/dornol/ticket/admin/api/app/controller/site/SiteController.kt
@@ -4,12 +4,15 @@ import dev.dornol.ticket.admin.api.app.dto.common.request.toContext
 import dev.dornol.ticket.admin.api.app.dto.site.request.SiteAddRequestDto
 import dev.dornol.ticket.admin.api.app.dto.site.request.SiteSearchDto
 import dev.dornol.ticket.admin.api.app.service.site.SiteService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+
+private val log = KotlinLogging.logger {}
 
 @RequestMapping("/sites")
 @RestController
@@ -22,7 +25,9 @@ class SiteController(
         search: SiteSearchDto,
         @PageableDefault(sort = ["default"]) pageable: Pageable,
         @AuthenticationPrincipal jwt: Jwt
-    ) = siteService.search(search.toContext(jwt), pageable)
+    ) = siteService.search(search.toContext(jwt), pageable).also {
+        log.info { "sites: $it" }
+    }
 
     @GetMapping("/{id}")
     fun site(
